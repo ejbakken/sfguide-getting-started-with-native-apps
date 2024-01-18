be884d1d0f0e38c87fc11f534fd024f8dab77f2c
ejbakken <eric.bakken94@gmail.com>
2024-01-18 15:46:50 -0600

    add hello_world function to setup script

--- app/src/scripts/setup.sql
+++ app/src/scripts/setup.sql
@@ -29,6 +29,14 @@ packages = ('snowflake-snowpark-python','pandas','scikit-learn==1.1.1')
 imports = ('/libraries/udf.py')
 handler = 'udf.cal_distance';
 
+create or replace function app_instance_schema.hello_world()
+returns string
+language python
+runtime_version = '3.8'
+packages = ('snowflake-snowpark-python')
+imports = ('/libraries/udf.py')
+handler = 'udf.hello_world';
+
 -- Create Stored Procedure
 create or replace procedure app_instance_schema.billing_event(number_of_rows int)
 returns string
@@ -62,6 +70,7 @@ grant usage on schema app_instance_schema to application role app_instance_role;
 grant usage on function app_instance_schema.cal_lead_time(int,int,int) to application role app_instance_role;
 grant usage on procedure app_instance_schema.billing_event(int) to application role app_instance_role;
 grant usage on function app_instance_schema.cal_distance(float,float,float,float) to application role app_instance_role;
+grant usage on function app_instance_schema.hello_world() to application role app_instance_role;
 grant SELECT on view app_instance_schema.MFG_SHIPPING to application role app_instance_role;
 grant usage on streamlit app_instance_schema.streamlit to application role app_instance_role;
 grant usage on procedure app_instance_schema.update_reference(string, string, string) to application role app_instance_role;
